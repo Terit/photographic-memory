920fba3c388cbe29405deb15b8edcdbced2c215b
Michael LeGrice <legrice@gmail.com>
2014-11-19 22:16:37 +0000

    first commit created picture class

---
+++ app/models/picture.rb
@@ -0,0 +1,9 @@
+class Picture # < ActiveRecord::Base
+
+  # has_many :votes
+  # has_many :songs, through: :votes
+
+  # validates :song_title, presence: true
+  # validates :author, presence: true
+  # # validates :author, presence: true, length: { maximum: 25 }
+end
