e4839bb2017d4839d4d2886bfb35361862126236
Michael LeGrice <legrice@gmail.com>
2014-11-21 19:46:25 +0000

    added picture model

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,9 +1,5 @@
-class Picture # < ActiveRecord::Base
+class Picture < ActiveRecord::Base
 
-  # has_many :votes
-  # has_many :songs, through: :votes
+  belongs_to :game
 
-  # validates :song_title, presence: true
-  # validates :author, presence: true
-  # # validates :author, presence: true, length: { maximum: 25 }
 end
