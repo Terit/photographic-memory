20b4982a06ecc8452a393f078b281fcbed3b50fd
Michael LeGrice <legrice@gmail.com>
2014-11-22 04:25:38 +0000

    two cards match, and two cards mismatch... third card needs work

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -11,7 +11,6 @@ var pictures = ['Andrew Theriault','Ashley Theiss','Casey Sampson','Dave Hyatt',
  // DEALS WITH CARD FLIPPING LIBRARY IN JQUERY.FLIP.JS-------------
 
 function toggle_card (card) {
-  if(card.data('match') === false) {
       if(card.data('flipped') === false){
       card.flip(true);
       card.data('flipped', true);
@@ -20,7 +19,10 @@ function toggle_card (card) {
       card.data('flipped', false);
     }
   }
-}
+
+  function isSameCard(card1, card2) {                       
+    return card1.data('name') === card2.data('name');               // function to see if cards have same name/url
+  }
 
  // JQUERY LISTENING BEGINS-------------------------------------
 
@@ -41,7 +43,7 @@ $(document).ready(function() {
       $("#cards").append(row);
     };
     newCard.data('name', picture);
-    newCard.data('id', index);
+    newCard.data('id', index); 
     $(".row").append(newCard);
     var name_path = "url('images/" + picture + ".jpg')";
     newCard.find('.back').css("background-image", name_path);
@@ -49,61 +51,54 @@ $(document).ready(function() {
       trigger: 'manual'
     });
     newCard.data('flipped', false);
-    newCard.data('match', false);
+
     
   });
 
  // FLIPPING CARDS AND MATCHING LOGIC-------------------------------------
 
-  function isSameCard(card1, card2) {
-    return card1.data('id') === card2.data('id');
-  }
 
-  var clickHistory = ["none"];       // none does not have a name attribute, ??????fix later??????
 
-  $('.flip').on('click', function() {
+  $('.flip').on('click', function() {                              // calling the div with the flip class once clickedm STATE 1
 
-    var currentCard = $(this);
-    currentCard.addClass("phlipped");
+    var currentCard = $(this);                                    // setting current card to flip div
 
-    if($(this).data('match', false)) {
-      //var name = $(this).data('name');
+    if(!$(this).hasClass('match')) {                           //if card does not have class phlipped
 
-      clickHistory.push(currentCard);
+    currentCard.addClass("phlipped");                             // then assign to flip  
 
-      toggle_card($(this));
+      toggle_card($(this));                                       // this is what flips the card  STATE 2
 
-      var previousCard = clickHistory[clickHistory.length - 2];
-        // console.log("this "+$(this));
-        // console.log("previousCard "+previousCard);
+      var phlippedCards = $(".phlipped");                         // assigns a variable to an array containing all class phlipped elements  
 
-      if(currentCard.data('name') === previousCard.data('name')){
-
-        if(!isSameCard(currentCard, previousCard)){
-          previousCard.data('match',true);
-          currentCard.data('match',true);
-        }
-      } else {
-        setTimeout (
-          function() {
-            toggle_card($(previousCard));
-            toggle_card($(currentCard));
-          }, 1000 );
-      }
-
+      if(phlippedCards.length === 2){                             // STATE 2.5
         
-      // if(flipped_card === name) {
-      //   console.log("CARD1 & CARD2 are the same name");
-      //   if(card1.data("id") === !(card2.data("id"))){
-      //     console.log("CARD1 & CARD2 have same id");
+        if(isSameCard($(phlippedCards[0]),$(phlippedCards[1]))){    // if cards match go to STATE 4 
+          console.log('found a match');
+          phlippedCards.each(function (index,element){
+            $(element).addClass('match');
+            $(element).removeClass('phlipped');
+          });
+        } else {                                                     // STATE 3
+            setTimeout( function() {
+              phlippedCards.each(function (index,element){
+              toggle_card($(element))
+              $(element).removeClass("phlipped")
+            });
+          },1000 );
+        }
+      }
+    }
+        // setTimeout (
+        //   function() {
+        //     $(".phlipped").each(function (index,element){
+        //       toggle_card($(element));
+        //       $(element).removeClass("phlipped");
+        //     });
+        //     // toggle_card($(previousCard));
+        //     // toggle_card($(currentCard));
+        //   }, 1000 );
 
-      // }
-      //   flipped_card = "none";
-
-      // } else if(flipped_card === "none") {
-      //   flipped_card = name;
-
-  }
 });
 
 
